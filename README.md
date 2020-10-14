a6993226ba78c47eda08000548d085d61e987f2b
Sebastien Rinsoz <sebastien@yoctopuce.com>
2020-10-14 09:58:19 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CMake Example 
+
+This is a simple example of usage of the Yoctopuce C++ library with CMake
---
+++ demo_app/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "yocto_api.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+  string      errmsg;
+
+  if(YAPI::RegisterHub("usb", errmsg) != YAPI::SUCCESS) {
+    cerr << "RegisterHub error: " << errmsg << endl;
+    return 1;
+  }
+
+  cout << "Device list: " << endl;
+  YModule *module = YModule::FirstModule();
+  while (module != NULL) {
+    cout << module->get_serialNumber() << " ";
+    cout << module->get_productName()  << endl;
+    module = module->nextModule();
+  }
+  YAPI::FreeAPI();
+  return 0;
+}
